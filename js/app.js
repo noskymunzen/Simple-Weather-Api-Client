e59de83555bd0181643c21822d1d0e01cc6b3461
Ninoska <munzenmayer@outlook.com>
2022-01-26 17:26:30 -0300

    feat:limpiar alerta

--- js/app.js
+++ js/app.js
@@ -25,10 +25,14 @@ function buscarClima(e) {
 }
 
 function mostrarError(mensaje) {
-    console.log (mensaje)
+
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
 
     //crear alerta
     const alerta = document.createElement('div');
+
     alerta.classList.add('bg-red-100','border-red-400','text-red-700','px-4','py-3','rounded','max-w-md','mx-auto','mt-6','text-center');
 
     alerta.innerHTML =  `
@@ -37,4 +41,13 @@ function mostrarError(mensaje) {
     `;
     
     container.appendChild(alerta);
+
+    //eliminar alerta despúes de 5 seg
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+
+    }
+
+  
 }
