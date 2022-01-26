e3126210121fac20c8d44eea7f64861ea4986670
Ninoska <munzenmayer@outlook.com>
2022-01-26 16:36:34 -0300

    validar-formulario

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
 
+
 window.addEventListener('load', () => {
     formulario.addEventListener('submit', buscarClima);
 })
@@ -10,5 +11,19 @@ function buscarClima(e) {
 
     e.preventDefault();
 
-    //console.log('buscando el clima...')
+    //validar
+    const cuidad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if( ciudad === '' || pais === '') {
+        //HUBO UN ERROR
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+
+    }
+}
+
+function mostrarError(mensaje) {
+    console.log (mensaje)
 }
